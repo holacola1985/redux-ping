ebc4be2de217538d92b81b98b0b61983c4c60793
Franck Ernewein <franck.ernewein@gmail.com>
2016-04-07 17:48:17 +0200

    test(wall): add 2 items consecutively

--- test/reducers/wall.js
+++ test/reducers/wall.js
@@ -4,6 +4,7 @@ import {expect} from 'chai';
 
 describe('wall reducer', () => {
 
+
   it('should be initialize with empty array ', () => {
     expect(wall(undefined, {
       type: 'UNKNOW'  
@@ -19,6 +20,22 @@ describe('wall reducer', () => {
     })).items).to.be.deep.equal([
       {
         text: 'hello'
+      }
+    ]);
+  });
+
+  it('should aggregate 2 items', () => {
+    const state1 = wall(undefined, aggregate({
+      text: 'hello1'
+    }));
+    expect(wall(state1, aggregate({
+      text: 'hello2'
+    })).items).to.be.deep.equal([
+      {
+        text: 'hello1'
+      },
+      {
+        text: 'hello2'
       }
     ]);
   });
